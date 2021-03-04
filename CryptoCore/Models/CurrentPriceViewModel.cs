b58b6298f170c20b3e4c0f06592f67b53c24594c
Jason Walser <r0flcopt0rz10611@gmail.com>
2021-03-03 20:55:48 -0500

    FIxed stuff

---
+++ CryptoCore/Models/CurrentPriceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CryptoCore.Services.Binance;
+using System.Collections.Generic;
+
+
+namespace CryptoCore.Models
+{
+    public class CurrentPriceViewModel
+    {
+        public CurrentPriceViewModel()
+        {
+            Coins = new List<CoinModel>();
+        }
+        public List<CoinModel> Coins { get; set; }
+    }
+}
---
+++ CryptoCore/Services/Binance/BinanceClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CryptoCore.Services.Binance
+{
+    public class BinanceClient
+    {
+        private readonly HttpClient _client;
+        public BinanceClient(HttpClient Client)
+        {
+            _client = Client;
+        }
+        public async Task<List<CurrentPriceResponse>> GetCurrentPrice()
+        {
+            var result = await _client.GetAsync($"/api/v3/ticker/price");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var stringContent = await result.Content.ReadAsStringAsync();
+
+
+
+                var obj = JsonSerializer.Deserialize<List<CurrentPriceResponse>>(stringContent);
+
+                return obj;
+            }
+            else
+            {
+                throw new HttpRequestException("Bad doge :(");
+            }
+        }
+        public async Task<TwentyFourHourTickerReponse> GetTwentyFourHourTickerInfo()
+        {
+            var result = await _client.GetAsync($"/api/v3/ticker/24hr");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var stringContent = await result.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                };
+
+                var obj = JsonSerializer.Deserialize<TwentyFourHourTickerReponse>(stringContent, options);
+
+                return obj;
+            }
+            else
+            {
+                throw new HttpRequestException("Bad doge :(");
+            }
+        }
+    }
+}

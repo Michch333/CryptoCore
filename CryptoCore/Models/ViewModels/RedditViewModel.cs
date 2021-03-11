ee962f46d1bc3e4f4628f26f7d0ab268348a0506
Jason Walser <r0flcopt0rz10611@gmail.com>
2021-03-10 21:43:34 -0500

    added reddit view model

---
+++ CryptoCore/Models/ViewModels/RedditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CryptoCore.Services.Reddit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoCore.Models.ViewModels
+{
+    public class RedditViewModel
+    {
+        public RedditViewModel()
+        {
+            RedditPosts = new List<RedditModel>();
+        }
+        public List<RedditModel> RedditPosts { get; set; }
+    }
+}

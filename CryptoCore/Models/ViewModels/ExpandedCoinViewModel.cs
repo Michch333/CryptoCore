5dcfa2d7e551eb72d00871d056848c28dd2eb6e9
Jason Walser <r0flcopt0rz10611@gmail.com>
2021-03-15 21:15:25 -0400

    ok

---
+++ CryptoCore/Models/ViewModels/ExpandedCoinViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoCore.Models.ViewModels
+{
+    public class ExpandedCoinViewModel
+    {
+
+        public string CoinSymbol { get; set; }
+        public float Price { get; set; }
+        public double PriceChange { get; set; }
+        public float PriceChangePercent { get; set; }
+        public double UserHigh { get; set; }
+        public double UserLow { get; set; }
+        public int Count { get; set; }
+    }
+}
+
